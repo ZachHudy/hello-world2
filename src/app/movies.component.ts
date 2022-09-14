e85e10200d98c0e1f5abd98173e8719d2ba2de5f
zachhudy <zachhudy@gmail.com>
2022-09-14 15:55:28 -0400

    final of component before service created

---
+++ src/app/movies.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movies',
+    template: `<h2>{{ title }}</h2>
+                <ul>
+                    <li *ngFor="let movie of movies">
+                        {{ movie }}
+                    </li>
+                </ul>
+    `
+})
+
+
+export class MoviesComponent {
+    title = 'I Love Movies'
+    movies = ['The Big Lebowski', 'The Fifth Element', 'High Fidelity']
+
+    getTitle() {
+        return this.title;
+    }
+
+}
